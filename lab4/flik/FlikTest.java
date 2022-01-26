691c372e6b3c4b54d7639fe60f5b9e8ed3430c8b
Ryan Yao <ryanyaol@outlook.com>
2022-01-26 11:14:45 +0800

    lab4 completed

---
+++ lab4/flik/FlikTest.java
@@ -0,0 +1,17 @@
+package flik;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class FlikTest {
+
+    @Test
+    public void randomTest() {
+        int i = 0;
+        for (int j = 0; i < 500; ++i, ++j) {
+            assertTrue(i == j);
+            assertTrue(Flik.isSameNumber(i, j));
+        }
+    }
+}
+
