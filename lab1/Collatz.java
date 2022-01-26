4137c98c995156b73eefbf3992ba72a35d2ad6d9
Ryan Yao <ryanyaol@outlook.com>
2022-01-26 10:35:56 +0800

    corrected Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,17 +3,16 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n == 1) {
             return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        } else if (n % 2 == 0) {
+            return n / 2;
         } else {
-            return n * 2;
+            return n * 3 + 1;
         }
     }
-    
+
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
