53138b94a7153587899e44bbc5ae351763d37b94
Ryan Yao <ryanyaol@outlook.com>
2022-02-28 20:32:31 +0800

    created Lab7 BSTMap.java

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,54 @@
+package bstmap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K, V> implements Map61B<K, V> {
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("unsupported!");
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        throw new UnsupportedOperationException("unsupported!");
+    }
+
+    @Override
+    public V get(K key) {
+        throw new UnsupportedOperationException("unsupported!");
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("unsupported!");
+    }
+
+    @Override
+    public void put(K key, V value) {
+        throw new UnsupportedOperationException("unsupported!");
+    }
+
+    /** not required. */
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException("unsupported!");
+    }
+
+    /** not required. */
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException("unsupported!");
+    }
+
+    /** not required. */
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException("unsupported!");
+    }
+
+    /** not required. */
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException("unsupported!");
+    }
+}
